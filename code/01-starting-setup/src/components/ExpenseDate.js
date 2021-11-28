23941c0e35032af131dd526c68375b85f4729dd2
unknown <dtulpa16@yahoo.com>
2021-11-28 16:31:08 -0600

    Created Components For Displaying Expense Items. (Moved From App.js)

--- code/01-starting-setup/src/components/ExpenseDate.js
+++ code/01-starting-setup/src/components/ExpenseDate.js
@@ -7,7 +7,7 @@ const month = props.date.toLocaleString("en-US", { month: "long" });
     return(
         <div className = "expense-date">
         <div className="expense-date__month">{month}</div>
-        <div className=".expense-date__day">{day}</div>
+        <div className="expense-date__day">{day}</div>
         <div className="expense-date__year">{year}</div>
       </div>
     )
